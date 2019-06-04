93a2b412b01ff379b911110a8138fe0d9843f144
Leah Perri <leahmperri@physics.ucla.edu>
2019-06-04 16:09:59 -0700

    saving df_label to disk, this is last commit that will contain df_label manipulation prior to writing and loading to/from disk

--- reddit_model.py
+++ reddit_model.py
@@ -6,6 +6,7 @@ from pyspark.sql.types import StringType, ArrayType, IntegerType
 import re
 import string
 import json
+from pyspark.ml import PipelineModel
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
@@ -38,6 +39,8 @@ def sanitize(text):
     trigrams_str = ' '.join(trigrams)
     return [unigrams_str, bigrams_str, trigrams_str]
 
+
+
 def combineNgrams(ngrams):
     ''' given an array [unigrams (string), bigrams (string), trigrams (string)]
         return one array containing all words in the three strings of the input array '''
@@ -49,8 +52,13 @@ def combineNgrams(ngrams):
         ngrams_combined.append(item)
     return ngrams_combined
 
+
+
 sanitize_udf = udf(lambda x: sanitize(x), ArrayType(StringType()))
 combineNgrams_udf = udf(lambda x: combineNgrams(x), ArrayType(StringType()))
+remove_t3_from_link_udf = udf(lambda x: x.strip('t3_'), StringType())
+
+
 
 def main(context):
     """Main function takes a Spark SQL context."""
@@ -58,60 +66,39 @@ def main(context):
     submissions_df = context.read.parquet("submissions.parquet")
     labeled_data_df = context.read.parquet("labeled_data.parquet")
     comment_label_join = comments_df.join(labeled_data_df, comments_df.id == labeled_data_df._c0)
-    columns_to_drop = ['author', 'author_cakeday', 'controversiality', 'author_flair_css_class', 'can_gild', 'can_mod_post', 'collapsed', 'collapsed_reason', 'distinguished', 'edited', 'gilded', 'is_submitter', 'link_id', 'parent_id', 'permalink', 'retrieved_on', 'stickied', 'subreddit', 'subreddit_id', 'subreddit_type', '_c0', '_c1', '_c2']
-    df = comment_label_join.drop(*columns_to_drop)
-    df = df.withColumnRenamed('_c3', 'label_djt')
-    df = df.withColumn('body_sanitized', sanitize_udf(df.body))
-    df = df.withColumn('ngrams_combined', combineNgrams_udf(df.body_sanitized))
-    df = df.withColumn('poslabel', when(col("label_djt") == 1, 1).otherwise(0))
-    df = df.withColumn('neglabel', when(col("label_djt") == -1, 1).otherwise(0))
-    df = df.drop('body_sanitized')
-    df.show(5,False)
-    cv = CountVectorizer(inputCol="ngrams_combined", binary=True, outputCol="features", minDF=10.0)
-    model = cv.fit(df)
-    model.transform(df).show(truncate=False)
+    columns_to_drop = ['author', 'author_cakeday', 'controversiality', 'author_flair_css_class', 'can_gild', 'can_mod_post', 'collapsed', 'collapsed_reason', 'distinguished', 'edited', 'gilded', 'is_submitter', 'parent_id', 'permalink', 'retrieved_on', 'stickied', 'subreddit', 'subreddit_id', 'subreddit_type', '_c0', '_c1', '_c2']
+    df_labeled = comment_label_join.drop(*columns_to_drop)
+    df_labeled = df_labeled.withColumnRenamed('_c3', 'label_djt')
+    df_labeled = df_labeled.withColumn('body_sanitized', sanitize_udf(df_labeled.body))
+    df_labeled = df_labeled.withColumn('ngrams_combined', combineNgrams_udf(df_labeled.body_sanitized))
+    df_labeled = df_labeled.withColumn('poslabel', when(col("label_djt") == 1, 1).otherwise(0))
+    df_labeled = df_labeled.withColumn('neglabel', when(col("label_djt") == -1, 1).otherwise(0))
+    df_labeled = df_labeled.drop('body_sanitized')
+    df_labeled = df_labeled.withColumn('link_id_cleaned',remove_t3_from_link_udf(df_labeled.link_id))
+    df_labeled = df_labeled.drop('link_id')
+    df_labeled = df_labeled.withColumnRenamed('id','comment_id')
+    df_with_post_title = df_labeled.selectExpr('author_flair_text as state','comment_id','body','created_utc','label_djt','ngrams_combined','poslabel','neglabel','link_id_cleaned').join(submissions_df.selectExpr('title','id'), df_labeled.link_id_cleaned == submissions_df.id)
+    df_with_post_title = df_with_post_title.drop('id')
+    df_with_post_title.write.parquet("df_label.parquet")
 
+    #comment_columns_to_drop = ['author', 'link_id', 'author_cakeday', 'controversiality', 'author_flair_css_class', 'can_gild', 'can_mod_post', 'collapsed', 'collapsed_reason', 'distinguished', 'edited', 'gilded', 'is_submitter', 'parent_id', 'permalink', 'retrieved_on', 'stickied', 'subreddit', 'subreddit_id', 'subreddit_type']
+    #comments_df = comments_df.withColumn('link_id_cleaned',remove_t3_from_link_udf(comments_df.link_id))
+    #comments_df = comments_df.drop(*comment_columns_to_drop)
+    #comments_df = comments_df
+    #comments_df = comments_df.filter("body not like '%/s%'")
+    #comments_df = comments_df.filter("body not like '&gt;%'")
 
-    # Initialize two logistic regression models.
-    # labelCol is the column containing the label, and featuresCol is the column containing the features.
-    poslr = LogisticRegression(labelCol="poslabel", featuresCol="features", maxIter=10)
-    neglr = LogisticRegression(labelCol="neglabel", featuresCol="features", maxIter=10)
-    # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
-    posEvaluator = BinaryClassificationEvaluator()
-    negEvaluator = BinaryClassificationEvaluator()
-    # There are a few parameters associated with logistic regression. We do not know what they are a priori.
-    # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
-    # We will assume the parameter is 1.0. Grid search takes forever.
-    posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
-    negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
-    # We initialize a 5 fold cross-validation pipeline.
-    posCrossval = CrossValidator(
-        estimator=poslr,
-        evaluator=posEvaluator,
-        estimatorParamMaps=posParamGrid,
-        numFolds=5)
-    negCrossval = CrossValidator(
-        estimator=neglr,
-        evaluator=negEvaluator,
-        estimatorParamMaps=negParamGrid,
-        numFolds=5)
-    # Although crossvalidation creates its own train/test sets for
-    # tuning, we still need a labeled test set, because it is not
-    # accessible from the crossvalidator (argh!)
-    # Split the data 50/50
-    posTrain, posTest = pos.randomSplit([0.5, 0.5])
-    negTrain, negTest = neg.randomSplit([0.5, 0.5])
-    # Train the models
-    print("Training positive classifier...")
-    posModel = posCrossval.fit(posTrain)
-    print("Training negative classifier...")
-    negModel = negCrossval.fit(negTrain)
-
-# Once we train the models, we don't want to do it again. We can save the models and load them again later.
-posModel.save("pos.model")
-negModel.save("neg.model")
-
-
+    #cv = CountVectorizer(inputCol="ngrams_combined", binary=True, outputCol="features", minDF=10.0)
+    #cv_model = cv.fit(df_with_post_title)
+   
+    #output = cv_model.transform(df_with_post_title)
+    
+    #output = cv_model.transform(comments_df)
+    #output.show(2)
+    
+    # load LR models
+    #pos_model = PipelineModel.load(pos_model)
+    #neg_model = PipelineModel.load(neg_model)
 
 
 if __name__ == "__main__":
